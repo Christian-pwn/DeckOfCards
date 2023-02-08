2d4a883646a044fb32dfdf7d6f5a75feaae7af5b
Christoph B. Wurzinger <chbw@users.noreply.github.com>
2023-02-08 20:58:05 +0100

    randomize deck indirectly

--- docw.js
+++ docw.js
@@ -1,4 +1,15 @@
-
+// 13 spades (Ace, 2, 3, ..., J, Q, K)
+// 13 hearts
+// 1 joker (red)
+// 13 diamonds
+// 13 clubs
+// 1 joker (white)
 var deck = '🂡🂢🂣🂤🂥🂦🂧🂨🂩🂪🂫🂭🂮🂱🂲🂳🂴🂵🂶🂷🂸🂹🂺🂻🂽🂾🂿🃁🃂🃃🃄🃅🃆🃇🃈🃉🃊🃋🃍🃎🃑🃒🃓🃔🃕🃖🃗🃘🃙🃚🃛🃝🃞🃟'
-var randomdeck = Array.from(deck)
+var deckarray = Array.from(deck)
+var randomdeck = Array.apply(0, Array(54)).map((element, index) => index)
 randomdeck.sort((a, b) => Math.random() - 0.5)
+// randomdeck.map((element)=>deck[element])
+
+function docwStart() {
+    alert('started')
+}
